b82c2d46eb5c646a8b54d7d2733212644ba626fc
Rich Boag <Richard.Boag@blndspt.com>
2019-02-12 11:37:23 -0700

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -43,8 +43,8 @@ namespace GradeBook.UserInterfaces
             BaseGradeBook gradeBook;
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
-            else if (type = "ranked")
-                gradeBook = new StandardGradeBook(name);
+            else if (type == "ranked")
+                gradeBook = new RankedGradeBook(name);
             else
             {
                 Console.WriteLine("{0} is not a supported type of gradebook, please try again.", type);
@@ -76,7 +76,7 @@ namespace GradeBook.UserInterfaces
         {
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.",Type);
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.",type);
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
